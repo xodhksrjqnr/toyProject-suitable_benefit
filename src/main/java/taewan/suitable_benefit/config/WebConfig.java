fecffa2dd1744f89529fb4756800d2c1df610725
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-08-13 17:29:40 +0900

    #6 modify upload->access

--- src/main/java/taewan/suitable_benefit/config/WebConfig.java
+++ src/main/java/taewan/suitable_benefit/config/WebConfig.java
@@ -8,15 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    //application.properties에 정의된 데이터를 호출
     @Value("${resource.path}")
     private String resourcePath;
 
-    @Value("${upload.path}")
-    private String uploadPath;
+    //application.properties에 정의된 데이터를 호출
+    @Value("${access.path}")
+    private String accessPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(uploadPath)
+        registry.addResourceHandler(accessPath)
                 .addResourceLocations(resourcePath + "img/");
     }
 }

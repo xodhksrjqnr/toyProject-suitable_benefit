3c5507825d7a2fb5ae9fed8155223e7ad183fa9a
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-08-22 17:24:21 +0900

    #12 PostDAO save method 반환 타입 수정

--- Admin/src/main/java/taewan/SBadmin/dao/PostDAO.java
+++ Admin/src/main/java/taewan/SBadmin/dao/PostDAO.java
@@ -1,18 +1,24 @@
 package taewan.SBadmin.dao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import taewan.SBadmin.entity.Post;
 import taewan.SBadmin.repository.PostRepository;
 
+@Slf4j
 @Component
 public class PostDAO {
 
-    @Autowired
-    PostRepository postRepository;
+    private PostRepository postRepository;
 
-    public void save(Post post) {
-        postRepository.save(post);
+    @Autowired
+    public PostDAO(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post save(Post post) {
+        return postRepository.save(post);
     }
 
     public void delete(Long postId) {

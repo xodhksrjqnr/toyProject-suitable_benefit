fd8483713989d7f1c96514d1caf6a9ed7ca57a28
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-08-22 16:03:22 +0900

    #12 PostDAO regiter & delete 구현

---
+++ Admin/src/main/java/taewan/SBadmin/dao/PostDAO.java
@@ -0,0 +1,21 @@
+package taewan.SBadmin.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import taewan.SBadmin.entity.Post;
+import taewan.SBadmin.repository.PostRepository;
+
+@Component
+public class PostDAO {
+
+    @Autowired
+    PostRepository postRepository;
+
+    private void register(Post post) {
+        postRepository.save(post);
+    }
+
+    private void delete(Long postId) {
+        postRepository.deleteById(postId);
+    }
+}

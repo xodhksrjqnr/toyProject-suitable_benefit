7f9e2720546bd192460d1a1d69fc316fb5f24f35
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-08-24 17:06:59 +0900

    #19 postDao 테스트 구현

--- Admin/src/test/java/taewan/SBadmin/post/PostDaoTest.java
+++ Admin/src/test/java/taewan/SBadmin/post/PostDaoTest.java
@@ -1,36 +1,51 @@
 package taewan.SBadmin.post;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import taewan.SBadmin.dao.PostDAO;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import taewan.SBadmin.config.AppConfig;
+import taewan.SBadmin.dao.PostDao;
 import taewan.SBadmin.entity.Post;
-import taewan.SBadmin.repository.PostRepository;
 
 import java.time.LocalDateTime;
 
-@DataJpaTest
-public class PostDAOTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    @Autowired
-    PostRepository postRepository;
-    PostDAO postDAO = new PostDAO();
+@SpringBootTest
+@Slf4j
+public class PostDaoTest {
 
+    private ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    private PostDao postDao = ac.getBean(PostDao.class);
+
+//    @BeforeTestClass
+//    public void setUp() {
+//        ac = new AnnotationConfigApplicationContext(AppConfig.class);
+//        postDao = ac.getBean("postDao", PostDao.class);
+//    }
     @Test
     public void 게시물저장테스트() {
         //given
         Post post = Post.builder()
                 .title("test post title")
-                .content("1. test\n2. test\n3.test")
+                .content("1. test\n2. test\n3. test")
                 .imgPath("test img path")
                 .expirationDate(LocalDateTime.now().plusDays(3L))
                 .needConditions(Long.parseLong("00111111111111111101111111111111111111111", 2))
+                .needDocuments(Long.parseLong("00111111111111111101111111111111111111111", 2))
+                .procedure("test procedure")
                 .build();
 
         //when
-        postDAO.save(post);
+        Post save = postDao.save(post);
 
         //then
+        assertThat(save.toString()).isEqualTo(post.toString());
+        log.info(save.toString());
+        log.info(post.toString());
     }
 }

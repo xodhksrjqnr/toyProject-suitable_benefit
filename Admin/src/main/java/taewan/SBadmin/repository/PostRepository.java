a9660bf03f1ea6054f784beb77982a020e0bd855
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-08-26 14:44:14 +0900

    #18 slice & countAllBy 메서드 추가

--- Admin/src/main/java/taewan/SBadmin/repository/PostRepository.java
+++ Admin/src/main/java/taewan/SBadmin/repository/PostRepository.java
@@ -1,12 +1,14 @@
 package taewan.SBadmin.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import taewan.SBadmin.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Post save(Post post);
     void deleteByPostId(Long postId);
-    Page<Post> findAll(Pageable pageable);
+    Slice<Post> findSliceBy(Pageable pageable);
+
+    long countAllBy();
 }

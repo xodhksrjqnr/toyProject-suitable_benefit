933699f523c6b26e6858f09d816e759cfc35fa93
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-08-27 00:00:35 +0900

    #19 게시물수정테스트 & 게시물삭제테스트 추가, dto를 생성하는 Utils 클래스 추가

--- Admin/src/test/java/taewan/SBadmin/post/PostDaoTest.java
+++ Admin/src/test/java/taewan/SBadmin/post/PostDaoTest.java
@@ -6,37 +6,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Slice;
 import taewan.SBadmin.dao.PostDao;
+import taewan.SBadmin.dto.PostSaveDto;
 import taewan.SBadmin.dto.PostUpdateDto;
 import taewan.SBadmin.entity.Post;
 import taewan.SBadmin.repository.PostRepository;
 
-import java.time.LocalDateTime;
+import javax.transaction.Transactional;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 public class PostDaoTest {
 
     @Autowired
     PostRepository postRepository;
     @Autowired
     PostDao postDao;
+    Utils utils = new Utils();
 
     @Test
     public void 게시물저장테스트() {
         //given
-        List<Post> posts = createPost(5);
+        int size = 5;
+        List<PostSaveDto> posts = new ArrayList<>(size);
+        for (int i = 0; i < size; i++)
+            posts.add(utils.createSaveDto(i));
         List<Post> savedPosts = new LinkedList<>();
 
         //when
-        for (Post post : posts)
+        for (PostSaveDto post : posts)
             savedPosts.add(postDao.save(post));
 
         //then
-        assertThat(savedPosts.size()).isEqualTo(5);
-        for (int i = 0; i < 5; i++) {
-            Post post = posts.get(i);
+        assertThat(savedPosts.size()).isEqualTo(size);
+        for (int i = 0; i < size; i++) {
+            Post post = new Post();
+            post.init(posts.get(i));
             Post save = savedPosts.get(i);
             assertThat(post.toString()).isEqualTo(save.toString());
         }
@@ -45,9 +52,12 @@ public class PostDaoTest {
     @Test
     public void 게시물조회테스트bySlice() {
         //given
-        List<Post> posts = createPost(18);
+        int size = 18;
+        List<PostSaveDto> posts = new ArrayList<>(18);
+        for (int i = 0; i < size; i++)
+            posts.add(utils.createSaveDto(size));
 
-        for (Post post : posts)
+        for (PostSaveDto post : posts)
             postDao.save(post);
 
         //when
@@ -56,39 +66,36 @@ public class PostDaoTest {
 
         //then
         assertThat(sliced1.getNumberOfElements()).isEqualTo(10);
-        assertThat(sliced2.getNumberOfElements()).isEqualTo(8);
+        assertThat(sliced2.getNumberOfElements()).isEqualTo(size - 10);
     }
 
     @Test
     public void 게시물삭제테스트() {
         //given
-        List<Post> posts = createPost(1);
-        Post save = postDao.save(posts.get(0));
+        Post save = postDao.save(utils.createSaveDto(1));
 
         //when
+        assertThat(postRepository.count()).isEqualTo(1);
         postDao.delete(save.getPostId());
 
         //then
         assertThat(postRepository.count()).isEqualTo(0);
     }
 
-    private List<Post> createPost(int num) {
-        List<Post> posts = new ArrayList<>(num);
+    @Test
+    public void 게시물수정테스트() {
+        //given
+        Post saved = postDao.save(utils.createSaveDto(1));
 
-        while (num-- > 0) {
-            PostUpdateDto postUpdateDto = PostUpdateDto.builder()
-                    .title("test post title" + num)
-                    .content("1. test\n2. test\n3. test")
-                    .imgPath("test img path")
-                    .expirationDate(LocalDateTime.now().plusDays(3L))
-                    .needConditions(Long.parseLong("00111111111111111101111111111111111111111", 2))
-                    .needDocuments(Long.parseLong("00111111111111111101111111111111111111111", 2))
-                    .procedure("test procedure")
-                    .build();
-            Post post = new Post();
-            post.init(postUpdateDto);
-            posts.add(post);
-        }
-        return posts;
+        //when
+        String str = saved.toString();
+        PostUpdateDto post = utils.createUpdateDto(1, saved.getPostId());
+        post.setContent(saved.getContent() + " diff");
+        postDao.modify(post);
+
+        //then
+        assertThat(str).isNotEqualTo(postDao.findOneByPostID(post.getPostId()).toString());
     }
+
+
 }

9be71ab0e4dbbcf71a8368db879d37e850ffcc4a
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-08-29 14:50:46 +0900

    #23 mainController 구현

--- Admin/src/main/java/taewan/SBadmin/controller/MainController.java
+++ Admin/src/main/java/taewan/SBadmin/controller/MainController.java
@@ -1,13 +1,40 @@
 package taewan.SBadmin.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import taewan.SBadmin.dto.PostFullInfoDto;
+import taewan.SBadmin.dto.PostSaveDto;
+import taewan.SBadmin.dto.PostSimpleInfoDto;
+import taewan.SBadmin.service.PostService;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class MainController {
 
-    @GetMapping("")
-    public String mainPage() {
-        return "base";
+    private final PostService postService;
+
+    @Autowired
+    public MainController(PostService postService) {
+        this.postService = postService;
     }
+
+    @GetMapping(value = {"posts/search/{page}", "posts/search"})
+    @ResponseBody
+    public List<PostSimpleInfoDto> search(@PathVariable(required = false) Integer page) {
+        return postService.searchAll(page);
+    }
+
+    @GetMapping("posts/{postId}")
+    @ResponseBody
+    public PostFullInfoDto searchOne(@PathVariable Long postId) {
+        return postService.searchOne(postId);
+    }
+
+    @PostMapping("posts/upload")
+    public String upload(PostSaveDto postSaveDto) {
+        postService.upload(postSaveDto);
+        return "redirect:/";
+    }
+
 }

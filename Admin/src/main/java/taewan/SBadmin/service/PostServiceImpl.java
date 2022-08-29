de02ec9a0c1cd2a9234ab08f4409790aa9c754cd
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-08-29 13:30:10 +0900

    #13 PostServiceImpl & PostService 구현

---
+++ Admin/src/main/java/taewan/SBadmin/service/PostServiceImpl.java
@@ -0,0 +1,47 @@
+package taewan.SBadmin.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import taewan.SBadmin.dao.PostDao;
+import taewan.SBadmin.dto.PostFullInfoDto;
+import taewan.SBadmin.dto.PostSaveDto;
+import taewan.SBadmin.dto.PostUpdateDto;
+
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final PostDao postDao;
+
+    @Autowired
+    public PostServiceImpl(PostDao postDao) {
+        this.postDao = postDao;
+    }
+
+    @Override
+    public Long upload(PostSaveDto postSaveDto) {
+        PostFullInfoDto saved = postDao.save(postSaveDto);
+        return saved.getPostId();
+    }
+
+    @Override
+    public List<PostFullInfoDto> searchAll(Integer page) {
+        return postDao.findAll(page != null ? page : 0);
+    }
+
+    @Override
+    public PostFullInfoDto searchOne(Long postId) {
+        return postDao.findOneByPostId(postId);
+    }
+
+    @Override
+    public void remove(Long postId) {
+        postDao.delete(postId);
+    }
+
+    @Override
+    public void update(PostUpdateDto postUpdateDto) {
+        postDao.modify(postUpdateDto);
+    }
+}

cd976334727b24c0978a256965b2db9d6e97e35a
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-09-01 23:31:37 +0900

    MainController -> ViewController

--- SB_Server/src/main/java/taewan/SBadmin/controller/ViewController.java
+++ SB_Server/src/main/java/taewan/SBadmin/controller/ViewController.java
@@ -1,39 +1,5 @@
 package taewan.SBadmin.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import taewan.SBadmin.dto.post.PostFullInfoDto;
-import taewan.SBadmin.dto.post.PostSaveDto;
-import taewan.SBadmin.dto.post.PostSimpleInfoDto;
-import taewan.SBadmin.service.PostService;
-
-import java.util.List;
-
-@RestController
-public class MainController {
-
-    private final PostService postService;
-
-    @Autowired
-    public MainController(PostService postService) {
-        this.postService = postService;
-    }
-
-    @GetMapping(value = {"posts/search/{page}", "posts/search"})
-    @ResponseBody
-    public List<PostSimpleInfoDto> search(@PathVariable(required = false) Integer page) {
-        return postService.searchAll(page);
-    }
-
-    @GetMapping("posts/{postId}")
-    @ResponseBody
-    public PostFullInfoDto searchOne(@PathVariable Long postId) {
-        return postService.searchOne(postId);
-    }
-
-    @PostMapping("posts/upload")
-    public Long upload(PostSaveDto postSaveDto) {
-        return postService.upload(postSaveDto);
-    }
+public class ViewController {
 
 }

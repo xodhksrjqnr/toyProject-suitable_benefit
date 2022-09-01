3d6f65e0f661259d5996c2b4c3fb57fa4a886ad2
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-09-01 17:37:27 +0900

    #33 MemberService 내용 추가

--- SB_Server/src/main/java/taewan/SBadmin/service/MemberService.java
+++ SB_Server/src/main/java/taewan/SBadmin/service/MemberService.java
@@ -1,4 +1,20 @@
 package taewan.SBadmin.service;
 
+import taewan.SBadmin.dto.member.MemberDto;
+import taewan.SBadmin.dto.member.MemberFullInfoDto;
+import taewan.SBadmin.dto.member.MemberSimpleInfoDto;
+
+import java.util.List;
+
 public interface MemberService {
+
+    public Long upload(MemberDto memberDto);
+
+    public List<MemberSimpleInfoDto> searchAll(Integer page);
+
+    public MemberFullInfoDto searchOne(Long memberId);
+
+    public void remove(Long memberId);
+
+    public void update(MemberDto memberDto);
 }
--- SB_Server/src/main/java/taewan/SBadmin/service/MemberServiceImpl.java
+++ SB_Server/src/main/java/taewan/SBadmin/service/MemberServiceImpl.java
@@ -3,6 +3,11 @@ package taewan.SBadmin.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import taewan.SBadmin.dao.MemberDao;
+import taewan.SBadmin.dto.member.MemberDto;
+import taewan.SBadmin.dto.member.MemberFullInfoDto;
+import taewan.SBadmin.dto.member.MemberSimpleInfoDto;
+
+import java.util.List;
 
 @Service
 public class MemberServiceImpl implements MemberService {
@@ -15,4 +20,29 @@ public class MemberServiceImpl implements MemberService {
     }
 
 
+    @Override
+    public Long upload(MemberDto memberDto) {
+        MemberFullInfoDto saved = memberDao.save(memberDto);
+        return saved.getMemberId();
+    }
+
+    @Override
+    public List<MemberSimpleInfoDto> searchAll(Integer page) {
+        return memberDao.findAll(page != null ? page : 0);
+    }
+
+    @Override
+    public MemberFullInfoDto searchOne(Long memberId) {
+        return memberDao.findOneByMemberId(memberId);
+    }
+
+    @Override
+    public void remove(Long memberId) {
+        memberDao.delete(memberId);
+    }
+
+    @Override
+    public void update(MemberDto memberUpdateDto) {
+        memberDao.modify(memberUpdateDto);
+    }
 }

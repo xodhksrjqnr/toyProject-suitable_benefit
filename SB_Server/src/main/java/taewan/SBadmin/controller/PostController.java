895120091c8c4a8e9803661e033247ccef569493
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-09-01 23:25:55 +0900

    #36 PostController 구현

---
+++ SB_Server/src/main/java/taewan/SBadmin/controller/PostController.java
@@ -0,0 +1,35 @@
+package taewan.SBadmin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import taewan.SBadmin.dto.PostFullInfoDto;
+import taewan.SBadmin.dto.PostSaveDto;
+import taewan.SBadmin.dto.PostSimpleInfoDto;
+import taewan.SBadmin.service.PostService;
+
+import java.util.List;
+
+@RestController(value = "posts")
+public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping(value = {"posts/search/{page}", "posts/search"})
+    public List<PostSimpleInfoDto> search(@PathVariable(required = false) Integer page) {
+        return postService.searchAll(page);
+    }
+
+    @GetMapping("posts/{postId}")
+    public PostFullInfoDto searchOne(@PathVariable Long postId) {
+        return postService.searchOne(postId);
+    }
+
+    @PostMapping("posts/upload")
+    public Long upload(PostSaveDto postSaveDto) {
+        return postService.upload(postSaveDto);
+    }
+}

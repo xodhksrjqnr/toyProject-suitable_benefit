991161904b0b8d344b5d46bdfe0ac0368e85d1ec
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-09-01 23:26:45 +0900

    #23 MainController 내용 제거

--- SB_Server/src/main/java/taewan/SBadmin/controller/MainController.java
+++ SB_Server/src/main/java/taewan/SBadmin/controller/MainController.java
@@ -12,28 +12,6 @@ import java.util.List;
 @RestController
 public class MainController {
 
-    private final PostService postService;
 
-    @Autowired
-    public MainController(PostService postService) {
-        this.postService = postService;
-    }
-
-    @GetMapping(value = {"posts/search/{page}", "posts/search"})
-    @ResponseBody
-    public List<PostSimpleInfoDto> search(@PathVariable(required = false) Integer page) {
-        return postService.searchAll(page);
-    }
-
-    @GetMapping("posts/{postId}")
-    @ResponseBody
-    public PostFullInfoDto searchOne(@PathVariable Long postId) {
-        return postService.searchOne(postId);
-    }
-
-    @PostMapping("posts/upload")
-    public Long upload(PostSaveDto postSaveDto) {
-        return postService.upload(postSaveDto);
-    }
 
 }

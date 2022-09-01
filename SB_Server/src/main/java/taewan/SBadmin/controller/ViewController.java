ea08c5c8c0fdb6af4d07a55e82d5aeb062fd4f6b
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-09-01 23:22:19 +0900

    #35 MemberController 구현

--- SB_Server/src/main/java/taewan/SBadmin/controller/ViewController.java
+++ SB_Server/src/main/java/taewan/SBadmin/controller/ViewController.java
@@ -2,9 +2,9 @@ package taewan.SBadmin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import taewan.SBadmin.dto.PostFullInfoDto;
-import taewan.SBadmin.dto.PostSaveDto;
-import taewan.SBadmin.dto.PostSimpleInfoDto;
+import taewan.SBadmin.dto.post.PostFullInfoDto;
+import taewan.SBadmin.dto.post.PostSaveDto;
+import taewan.SBadmin.dto.post.PostSimpleInfoDto;
 import taewan.SBadmin.service.PostService;
 
 import java.util.List;

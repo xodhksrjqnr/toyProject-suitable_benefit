3bae00605e30114cf2dbd692e6405c5eb49e9e7b
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-09-23 13:41:38 +0900

    #5 uploadForm 임시 형식 적용

--- SB_front_admin/src/js/content/UploadForm.js
+++ SB_front_admin/src/js/content/UploadForm.js
@@ -8,26 +8,36 @@ import RequiredInfo from "./RequiredInfo";
 function UploadForm() {
     return (
         <Container className="p-4">
-            <form className="uploadForm p-4 bg-white">
+            <form className="uploadForm p-4 bg-white" id="postUploadForm" method="post" action="http://localhost:8080/posts/upload">
                 <ImagePreview/>
                 <div>
                     <label htmlFor="title">제목</label>
-                    <input type="text" id="title" className=""/>
+                    <input type="text" id="title" name="title"/>
                 </div>
                 <div>
                     <label htmlFor="content">내용</label>
-                    <input type="text" id="content"/>
+                    <input type="text" id="content" name="content"/>
                 </div>
                 <div>
                     <label htmlFor="expirationDate">만료일</label>
-                    <input type="date" id="expirationDate"/>
+                    <input type="datetime-local" id="expirationDate" name="expirationDate"/>
                 </div>
-                <RequiredInfo need="needConditions" name="필요 조건"/>
-                <RequiredInfo need="needDocuments" name="필요 서류"/>
                 <div>
-                    <p>procedure</p>
+                    <label htmlFor="needConditions">필요 조건</label>
+                    <input type="number" id="needConditions" name="needConditions"/>
+                </div>
+                <div>
+                    <label htmlFor="needDocuments">필요 서류</label>
+                    <input type="number" id="needDocuments" name="needDocuments"/>
+                </div>
+                {/*<RequiredInfo need="needConditions" name="필요 조건"/>*/}
+                {/*<RequiredInfo need="needDocuments" name="필요 서류"/>*/}
+                <div>
+                    <label htmlFor="procedure">절차</label>
+                    <input type="text" id="procedure" name="procedure"/>
                 </div>
             </form>
+            <button type="submit" form="postUploadForm">등록하기</button>
         </Container>
     );
 }

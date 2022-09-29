fb035ade2fcc57b19aacdaa99875f47895ade05c
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-09-29 13:20:20 +0900

    임시 upload api 추가

--- SB_Server/src/main/java/taewan/SBadmin/controller/PostController.java
+++ SB_Server/src/main/java/taewan/SBadmin/controller/PostController.java
@@ -10,6 +10,7 @@ import taewan.SBadmin.service.PostService;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("posts")
@@ -40,5 +41,21 @@ public class PostController {
         System.out.println(postSaveDto.getNeedDocuments().byteValue());
         return 10L;
 //        return postService.upload(postSaveDto);
+    }
+
+    @GetMapping("/tmpupload/{num}")
+    public void tmpUpload(@PathVariable int num) {
+        Random random = new Random();
+        for (int i = 0; i < num; i++) {
+            String title = "title" + i;
+            String imgPath = "https://www.next-t.co.kr/public/uploads/7b7f7e2138e29e598cd0cdf2c85ea08d.jpg";
+            String content = "1.test를 위해 입력된 내용입니다./n2.내용의 형식은 대체로 같으며 게시물의 숫자에 따른 차이가 있습니다./n감사합니다." + i;
+            String url = "https://test-url" + i + ".com";
+            LocalDateTime expirationDate = LocalDateTime.now().plusDays(i + 1);
+            Long needConditions = Math.abs(random.nextLong());
+            Long needDocuments = Math.abs(random.nextLong());
+            PostSaveDto postSaveDto = new PostSaveDto(title, imgPath, content, expirationDate, needConditions, needDocuments, url);
+            postService.upload(postSaveDto);
+        }
     }
 }

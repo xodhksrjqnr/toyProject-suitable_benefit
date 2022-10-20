0f9af81fb6a4dcef17e754fc16e64f56813fd59c
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-10-20 16:52:31 +0900

    #43 임시 컨트롤러(NeedDocumentController) 추가

---
+++ SB_Server/src/main/java/taewan/SBadmin/controller/NeedDocumentController.java
@@ -0,0 +1,24 @@
+package taewan.SBadmin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import taewan.SBadmin.dao.NeedDocumentDao;
+import taewan.SBadmin.entity.NeedDocument;
+
+public class NeedDocumentController {
+    private final NeedDocumentDao needDocumentDao;
+
+    @Autowired
+    public NeedDocumentController(NeedDocumentDao needDocumentDao) {
+        this.needDocumentDao = needDocumentDao;
+    }
+
+    @GetMapping("/tmpNeedConditions")
+    public void tmpUpload() {
+        for (int i = 0; i < 60; i++) {
+            NeedDocument tmp = new NeedDocument();
+            tmp.setName("test" + i);
+            needDocumentDao.save(tmp);
+        }
+    }
+}

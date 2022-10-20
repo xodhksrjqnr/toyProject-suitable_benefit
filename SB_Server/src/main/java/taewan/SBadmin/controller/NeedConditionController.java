e8f481e56b9878f4417a0cfb6bebaa33a4d188e6
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-10-20 16:33:09 +0900

    임시컨트롤러 (NeedConditionController) 추가

---
+++ SB_Server/src/main/java/taewan/SBadmin/controller/NeedConditionController.java
@@ -0,0 +1,27 @@
+package taewan.SBadmin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import taewan.SBadmin.dao.NeedConditionDao;
+import taewan.SBadmin.entity.NeedCondition;
+
+@RestController
+public class NeedConditionController {
+
+    private final NeedConditionDao needConditionDao;
+
+    @Autowired
+    public NeedConditionController(NeedConditionDao needConditionDao) {
+        this.needConditionDao = needConditionDao;
+    }
+
+    @GetMapping("/tmpNeedConditions")
+    public void tmpUpload() {
+        for (int i = 0; i < 60; i++) {
+            NeedCondition tmp = new NeedCondition();
+            tmp.setName("test" + i);
+            needConditionDao.save(tmp);
+        }
+    }
+}
--- SB_Server/src/main/java/taewan/SBadmin/controller/PostController.java
+++ SB_Server/src/main/java/taewan/SBadmin/controller/PostController.java
@@ -13,7 +13,6 @@ import java.util.Random;
 
 @RestController
 @RequestMapping("posts")
-@ResponseBody
 public class PostController {
     private final PostService postService;
 

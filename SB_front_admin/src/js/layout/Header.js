0655ab6983fb76fb607d78e7828720846cebc8b1
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-10-25 14:19:52 +0900

    #5 관리자 페이지 게시물 추가 기능 보완

--- SB_front_admin/src/js/layout/Header.js
+++ SB_front_admin/src/js/layout/Header.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Button from "react-bootstrap/Button";
 
 import '../../css/Header.css'
 
@@ -11,9 +10,9 @@ function Header() {
             <Col sm={2} className="p-1">Logo</Col>
             <Col sm={10} className="p-1 bg-white">
                 <Row>
-                    <Col>AdminName</Col>
+                    <Col></Col>
                     <Col>
-                        <Button className="btn-secondary btn-sm">로그아웃</Button>
+                        {/*<Button className="btn-secondary btn-sm">로그아웃</Button>*/}
                     </Col>
                 </Row>
             </Col>

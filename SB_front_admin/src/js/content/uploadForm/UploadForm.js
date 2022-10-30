4bf23d9c52cc6b7d1fb79f15706047a57cbd9e7d
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-10-30 14:42:11 +0900

    #5 UploadForm, ImagePreview 통합

--- SB_front_admin/src/js/content/uploadForm/UploadForm.js
+++ SB_front_admin/src/js/content/uploadForm/UploadForm.js
@@ -2,14 +2,26 @@ import Container from "react-bootstrap/Container";
 
 import '../../../css/UploadForm.css'
 
-import ImagePreview from "./ImagePreview";
 import RequiredInfo from "./requiredInfo/RequiredInfo";
+import {useState} from "react";
+import Image from "react-bootstrap/Image";
 
 function UploadForm() {
+    const [imgPath, setImgPath] = useState('/img/noImg.png');
+
+    const renderPreview = () => setImgPath(document.getElementById('imgPath').value);
+
     return (
-        <Container className="p-4">
+        <Container className="p-4 bg-white">
+            <div className="text-center mb-5">
+                <Image src={imgPath} className="img-fluid"/>
+            </div>
             <form className="uploadForm p-4 bg-white" id="postUploadForm" method="post" action="http://localhost:8080/posts/upload">
-                <ImagePreview/>
+                <div>
+                    <label htmlFor="imgPath">이미지</label>
+                    <input type="text" id="imgPath" name="imgPath"/>
+                    <button type="button" onClick={() => renderPreview()}>Preview</button>
+                </div>
                 <div>
                     <label htmlFor="title">제목</label>
                     <input type="text" id="title" name="title"/>

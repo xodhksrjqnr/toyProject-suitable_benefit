30bade6caa649df274ee7552f7390fe9ef35d625
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-11-02 14:04:05 +0900

    #1 table 태그 적용 & pre 태그 적용으로 개행 처리

--- SB_front_client/src/js/content/Post.js
+++ SB_front_client/src/js/content/Post.js
@@ -12,7 +12,7 @@ function Post(props) {
             <div className="item explanation shadow">
                 <div>
                     <h6>무엇을 지원해주나요?</h6>
-                    <p>{props.post.content}</p>
+                    <pre>{props.post.content}</pre>
                 </div>
             </div>
             <div className="item expiration shadow">
@@ -20,7 +20,7 @@ function Post(props) {
                     <h6 style={{height:"5%"}}>누가 신청할 수 있나요?</h6>
                     <div style={{height:"85%"}}>
                         {props.post.convertedConditions.map(condition =>
-                            <div className="mx-1" style={{display:"inline-block", fontSize:"10px"}} key={condition}>{condition}</div>
+                            <div className="mx-1" style={{display:"inline-block", fontSize:"1rem"}} key={condition}>{condition}</div>
                         )}
                     </div>
                     <div style={{height:"5%", fontSize:"5px"}}>❉ 자세한 신청 조건을 꼭 확인하세요! ❉</div>
@@ -29,8 +29,16 @@ function Post(props) {
             <div className="item expiration shadow">
                 <div>
                     <h6>언제까지 신청해야 하나요?</h6>
-                    <Day expirationDate={props.post.expirationDate}/>
-                    <Clock expirationDate={props.post.expirationDate}/>
+                    <table className="w-100" style={{height:"75%"}}>
+                        <tbody>
+                            <tr style={{height:"50%"}}>
+                                <Day expirationDate={props.post.expirationDate}/>
+                            </tr>
+                            <tr style={{height:"50%"}}>
+                                <Clock expirationDate={props.post.expirationDate}/>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <div className="item button shadow">

cca3f47bb49f40843928200f0cba88de3c89558d
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-11-03 23:54:46 +0900

    #18 정의 메서드 제거

--- SB_Server/src/main/java/taewan/SBadmin/repository/PostRepository.java
+++ SB_Server/src/main/java/taewan/SBadmin/repository/PostRepository.java
@@ -10,12 +10,9 @@ import org.springframework.data.repository.query.Param;
 import taewan.SBadmin.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Post save(Post post);
-    void deletePostByPostId(Long postId);
     Slice<Post> findPostsBy(Pageable pageable);
     Slice<Post> findPostsByActivity(Pageable pageable, boolean activity);
     Slice<Post> findPostsByTagsAndActivity(Pageable pageable, long tags, boolean activity);
-    Post findPostByPostId(Long postId);
     @Modifying
     @Query(value = "update Post p set p.activity = if(p.activity, false, true) where p.postId = :postId")
     void modifyPostActivityByPostId(@Param("postId") Long postId);

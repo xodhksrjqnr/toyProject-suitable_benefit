b4520b9db518a973867d5867e3b47d4063fd397e
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-11-03 22:26:35 +0900

    #35 MemberController CrossOrigin 세분화

--- SB_Server/src/main/java/taewan/SBadmin/controller/MemberController.java
+++ SB_Server/src/main/java/taewan/SBadmin/controller/MemberController.java
@@ -10,7 +10,6 @@ import taewan.SBadmin.service.MemberService;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"${admin.origins}", "${client.origins}"})
 @RequestMapping(value = "members")
 public class MemberController {
 
@@ -21,16 +20,19 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @CrossOrigin(origins = "${admin.origins}")
     @GetMapping("/{page}")
     public List<MemberSimpleInfoDto> searchAll(@PathVariable(required = false) Integer page) {
         return memberService.searchAll(page);
     }
 
+    @CrossOrigin(origins = {"${admin.origins}", "${client.origins}"})
     @GetMapping("/{memberId}")
     public MemberFullInfoDto searchOne(@PathVariable Long memberId) {
         return memberService.searchOne(memberId);
     }
 
+    @CrossOrigin(origins = "${client.origins}")
     @PostMapping
     public Long upload(MemberDto memberSaveDto) {
         return memberService.upload(memberSaveDto);

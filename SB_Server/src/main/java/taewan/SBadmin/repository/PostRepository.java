585d28cdcbb247b5a3978de76155806c566f5c33
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-11-08 02:18:46 +0900

    #18 @Parameter -> ?

--- SB_Server/src/main/java/taewan/SBadmin/repository/PostRepository.java
+++ SB_Server/src/main/java/taewan/SBadmin/repository/PostRepository.java
@@ -1,8 +1,6 @@
 package taewan.SBadmin.repository;
 
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,16 +10,16 @@ import taewan.SBadmin.entity.Post;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query(nativeQuery = true, value = "select * from post where post_id >= :cursor limit 10")
-    List<Post> findPostsAll(@Param("cursor") int cursor);
+    @Query(nativeQuery = true, value = "select * from post where post_id >= ?1 limit 10")
+    List<Post> findPostsAll(int cursor);
     @Query(nativeQuery = true,
-            value = "select * from post where post_id >= :cursor and tags & :tags = :tags and activie = true limit 10")
-    List<Post> findPostsAllByTags(@Param("cursor") int cursor, @Param("tags") long tags);
+            value = "select * from post where post_id >= ?1 and tags & ?2 = ?2 and activie = true limit 10")
+    List<Post> findPostsAllByTags(int cursor, long tags);
     @Query(nativeQuery = true,
-            value = "select * from post where post_id >= :cursor and activity = true limit 10")
-    List<Post> findActivityPostsAll(@Param("cursor") int cursor);
+            value = "select * from post where post_id >= ?1 and activity = true limit 10")
+    List<Post> findActivityPostsAll(int cursor);
     @Modifying
-    @Query("update Post p set p.activity = if(p.activity, false, true) where p.postId = :id")
-    void modifyPostActivityByPostId(@Param("id") Long postId);
+    @Query("update Post p set p.activity = if(p.activity, false, true) where p.postId = ?1")
+    void modifyPostActivityByPostId(Long postId);
     long count();
 }

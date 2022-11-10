a53c37cdf9db5ebc722418eb338962e60ca35ea4
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-11-10 17:23:10 +0900

    Member 기능 제거(추후 구현 예정)

--- SB_Server/src/main/java/taewan/SBadmin/config/AppConfig.java
+++ SB_Server/src/main/java/taewan/SBadmin/config/AppConfig.java
@@ -3,14 +3,10 @@ package taewan.SBadmin.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import taewan.SBadmin.dao.MemberDao;
 import taewan.SBadmin.dao.TagDao;
 import taewan.SBadmin.dao.PostDao;
-import taewan.SBadmin.repository.MemberRepository;
 import taewan.SBadmin.repository.TagRepository;
 import taewan.SBadmin.repository.PostRepository;
-import taewan.SBadmin.service.MemberService;
-import taewan.SBadmin.service.MemberServiceImpl;
 import taewan.SBadmin.service.PostService;
 import taewan.SBadmin.service.PostServiceImpl;
 
@@ -18,14 +14,11 @@ import taewan.SBadmin.service.PostServiceImpl;
 public class AppConfig {
 
     private final PostRepository postRepository;
-    private final MemberRepository memberRepository;
     private final TagRepository tagRepository;
 
     @Autowired
-    public AppConfig(PostRepository postRepository, MemberRepository memberRepository,
-                     TagRepository tagRepository) {
+    public AppConfig(PostRepository postRepository, TagRepository tagRepository) {
         this.postRepository = postRepository;
-        this.memberRepository = memberRepository;
         this.tagRepository = tagRepository;
     }
 
@@ -37,16 +30,6 @@ public class AppConfig {
     @Bean
     public PostService postService() {
         return new PostServiceImpl(postDao(), tagDao());
-    }
-
-    @Bean
-    public MemberDao memberDao() {
-        return new MemberDao(memberRepository);
-    }
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberServiceImpl(memberDao());
     }
 
     @Bean

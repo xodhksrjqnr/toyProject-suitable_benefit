b830bf97439ebeaaf7e0aeb10b0b70006a512bd8
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-11-14 20:18:14 +0900

    #1 Percent 수정

--- SB_front_client/src/js/content/Percent.js
+++ SB_front_client/src/js/content/Percent.js
@@ -8,8 +8,8 @@ function Percent(props) {
     const getColor = (ratio) => {
         if (ratio === 0) return "gray";
         else if (ratio <= 25) return "#FF3232";
-        else if (ratio <= 50) return "#FFBE0A";
-        else if (ratio <= 89) return "#FAF58C";
+        else if (ratio <= 50) return "#FF8200";
+        else if (ratio <= 89) return "#FFEB46";
         else return "#9EF048";
     }
 
@@ -18,9 +18,9 @@ function Percent(props) {
         const numerator = ((props.userBit & props.postBit) >>> 0).toString(2)
             .replaceAll('0', '').length;
         const ratio = numerator / denominator;
-        const offset = (denominator === 0 ? 0 : (1 - ratio) * 214) + "%";
-        setPercent(<p style={{color:getColor(ratio * 100)}}>{ratio * 100}</p>);
-        setCircle(<circle style={{strokeDashoffset:{offset}, stroke:getColor(ratio * 100)}}/>);
+        const offset = (numerator === 0 ? 0 : Math.floor((1 - ratio) * 159));
+        setPercent(<p style={{color:getColor(ratio * 100)}}>{Math.floor(ratio * 100) + "%"}</p>);
+        setCircle(<circle style={{strokeDashoffset:offset, stroke:getColor(ratio * 100)}}/>);
     }, [props])
 
     return (

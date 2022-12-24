c0449a937a4213b521b12a612c1ad297f5b0c088
xodhksrjqnr <xodhksrjqnr@naver.com>
2022-12-24 11:24:58 +0900

    SB_Server 제거

--- SB_Server/src/main/java/taewan/SBadmin/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package taewan.SBadmin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import taewan.SBadmin.entity.Tag;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
